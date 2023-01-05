529681fda4e2958f69f78b024f51659cfacc65ec
okhunjon-2022 <okhunjonmakhmudov.1998@gmail.com>
2023-01-05 14:51:01 +0500

    upload buldi

--- Switch,loop/app.js
+++ Switch,loop/app.js
@@ -1,1 +1,68 @@
 console.log("Js is started");
+// if -> <,>,<=,>=,==,===
+//switch-> === only
+
+// let wheater = prompt();
+// switch (wheater) {
+//   case "rain":
+//     console.log("Today is rainy day");
+//     break;
+//   case "foggy":
+//     console.log("Today is foggy day");
+//     break;
+//   case "snow":
+//     console.log("Tody is snowy day");
+//     break;
+//   default:
+//     console.log("no information");
+//     break;
+// }
+
+// let dictionary = prompt();
+// switch (dictionary) {
+//   case "book":
+//     console.log("kitob");
+//     break;
+//   case "pen":
+//     console.log("qalam");
+//     break;
+//   case "car":
+//     console.log("kiton");
+//     break;
+//   case "traffic light":
+//     console.log("svetafor");
+//     break;
+//     default:
+//         console.log("So'z topilmadi");
+// }
+
+// for (i=10; i >=1; i = i - 1) {
+//   console.log(i);
+// }
+// let sum = 0;
+// for (i = 0; i < 60; i++) {
+//   console.log(`SUM = ${i} + ${(sum = sum + 1)}`);
+//   if (sum === 20) break;
+// }
+// let arr = [
+//   5_000, 10_000, 20_000, 50_000, 100_000, 5_000, 20_000, 50_000, 100_000,
+// ];
+// let sum = 0;
+// for (const value of arr) {
+//   console.log((sum = sum + value));
+
+// }
+//Juft sonlarni yigindisi
+
+// let sum = 0;
+// for (i = 0; i < 60; i++) {
+//   if(i % 2 === 0 )
+//   console.log(sum=sum + i);
+// }
+
+//toq sonlarni yigindisi
+// let sum = 0;
+// for (i = 0; i < 60; i++) {
+//   if(i % 2 !== 0 )
+//   console.log(sum=sum + i);
+// }
