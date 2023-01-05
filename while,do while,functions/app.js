1d47dfa5fe5ecc61eaed735c578b1144d80cac67
okhunjon-2022 <okhunjonmakhmudov.1998@gmail.com>
2023-01-05 15:43:13 +0500

    funkisiya

--- while,do while,functions/app.js
+++ while,do while,functions/app.js
@@ -1,1 +1,26 @@
 console.log("Js is started");
+while (false) {
+  console.log("Welcome");
+}
+
+function declaration(name,familiya) {
+    console.log(`Welcome to our country ${name} ${familiya}`);
+}
+declaration("Okhunjon","Mahmudov");
+let arrow = (name) => {
+    console.log(`Welcome to our country ${name}`);
+};
+arrow("Okhunjon")
+let expression =function(name){
+    console.log(`Welcome to our country ${name}`);
+};
+expression("Okhunjon")
+
+
+let name = "Ali"
+
+
+function telegram(name="",surname="") {
+    console.log(name,surname);
+}
+telegram("okhunjon")
