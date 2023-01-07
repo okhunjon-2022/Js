5679a2f8d66038045b8734912b6cac674c9401ab
okhunjon-2022 <okhunjonmakhmudov.1998@gmail.com>
2023-01-07 16:27:46 +0500

    Masala

--- Masalar yechish/execise.js
+++
@@ -1,7 +0,0 @@
-// 1. stringni barcha so'zlarni bosh harflarini katta qiladigon function yozing.
-function capitalLetter(str) {
-  // code here
-  return str.toUppercase();
-}
-console.log(capitalLetter("hello world")); // Hello World
-console.log(capitalLetter("Lorem ipsum dolor")); // Lorem Ipsum Dolor
